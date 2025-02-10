ae8162a106410dcd30d128dc412bd60e1614c255
ayushM <ayushmehrotra099@gmail.com>
2025-02-10 12:49:38 +0530

    changed completion icons:

--- lua/ayush/plugins/colorscheme.lua
+++ lua/ayush/plugins/colorscheme.lua
@@ -93,42 +93,6 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- Options: 'latte', 'frappe', 'macchiato', 'mocha'
-				transparent_background = false,
-				integrations = {
-					treesitter = true,
-					lsp_trouble = true,
-					cmp = true,
-					gitsigns = true,
-					telescope = true,
-					nvimtree = true,
-					dashboard = true,
-					markdown = true,
-					which_key = true,
-					indent_blankline = {
-						enabled = true,
-						colored_indent_levels = false,
-					},
-					native_lsp = {
-						enabled = true,
-						virtual_text = {
-							errors = "italic",
-							hints = "italic",
-							warnings = "italic",
-							information = "italic",
-						},
-						underlines = {
-							errors = "underline",
-							hints = "underline",
-							warnings = "underline",
-							information = "underline",
-						},
-					},
-				},
-			})
-		end,
 	},
 
 	-- Nord Theme
--- lua/ayush/plugins/nvim-cmp.lua
+++ lua/ayush/plugins/nvim-cmp.lua
@@ -2,68 +2,86 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+
 		{
 			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
+			version = "v2.*",
 			build = "make install_jsregexp",
 		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-
-		"neoclide/coc.nvim",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
+		"nvim-tree/nvim-web-devicons",
 	},
 
 	config = function()
 		local cmp = require("cmp")
-
 		local luasnip = require("luasnip")
-
 		local lspkind = require("lspkind")
+		local devicons = require("nvim-web-devicons")
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
+
+		-- Define static icons for LSP and snippets
+		local source_icons = {
+			nvim_lsp = " ", -- Gear (LSP)
+			luasnip = " ", -- Scissors (Snippet)
+			path = " ", -- Folder (Path)
+		}
+
+		-- Function to get buffer icons dynamically
+		local function get_buffer_icon(entry)
+			local filename = entry.completion_item.label
+			local extension = filename:match("^.+%.(.+)$") or ""
+			local icon, hl = devicons.get_icon(filename, extension, { default = true })
+			return icon or " " -- Default icon for unknown filetypes
+		end
+
+		-- Function to get LSP icons dynamically
+		local function get_lsp_icon(entry)
+			local kind = entry:get_kind()
+			local icon, _ = devicons.get_icon_by_filetype(kind) -- Try fetching LSP icon
+			return icon or " " -- Default icon for unknown LSP types
+		end
 
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
+			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
-			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
 			}),
-
-			-- configure lspkind for vs-code like pictograms in completion menu
-			cmp.formatting
-				== {
-
-					format = lspkind.cmp_format({
-						maxwidth = 50,
-						ellipsis_char = "...",
-					}),
-				},
+			formatting = {
+				format = function(entry, vim_item)
+					if entry.source.name == "buffer" then
+						vim_item.menu = get_buffer_icon(entry)
+					elseif entry.source.name == "nvim_lsp" then
+						vim_item.menu = get_lsp_icon(entry)
+					else
+						vim_item.menu = source_icons[entry.source.name] or ""
+					end
+					return vim_item
+				end,
+			},
 		})
 	end,
 }
