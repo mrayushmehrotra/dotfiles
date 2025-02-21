24444faa57d750442d4eb6ad8d0c45c5c02a22a4
theayushmehrotra <ayush@mehrotra>
2025-02-21 09:59:56 +0530

    added tsx jsx close tag

--- lua/ayush/plugins/autoclose.lua
+++ lua/ayush/plugins/autoclose.lua
@@ -1,17 +1,25 @@
 return {
-	"alvan/vim-closetag",
-	config = function()
-		vim.g.closetag_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
-		vim.g.closetag_xhtml_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
-		vim.g.closetag_filetypes = "html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
-		vim.g.closetag_xhtml_filetypes =
-			"html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
-		vim.g.closetag_regions = {
-			["typescript.tsx"] = "jsxRegion,tsxRegion",
-			["javascript.jsx"] = "jsxRegion",
-			["typescriptreact"] = "jsxRegion,tsxRegion",
-			["javascriptreact"] = "jsxRegion",
-		}
-		vim.g.closetag_shortcut = ">"
-	end,
+	{
+		"alvan/vim-closetag",
+		config = function()
+			vim.g.closetag_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
+			vim.g.closetag_xhtml_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
+			vim.g.closetag_filetypes = "html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
+			vim.g.closetag_xhtml_filetypes =
+				"html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
+			vim.g.closetag_regions = {
+				["typescript.tsx"] = "jsxRegion,tsxRegion",
+				["javascript.jsx"] = "jsxRegion",
+				["typescriptreact"] = "jsxRegion,tsxRegion",
+				["javascriptreact"] = "jsxRegion",
+			}
+			vim.g.closetag_shortcut = ">"
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 }
