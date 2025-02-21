9024950736fcc15eb6544e4f256ea2299df36563
theayushmehrotra <ayush@mehrotra>
2025-02-21 09:51:35 +0530

    autoclose tag add

---
+++ lua/ayush/plugins/autoclose.lua
@@ -0,0 +1,17 @@
+return {
+	"alvan/vim-closetag",
+	config = function()
+		vim.g.closetag_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
+		vim.g.closetag_xhtml_filenames = "*.html,*.xhtml,*.jsx,*.tsx,*.cshtml"
+		vim.g.closetag_filetypes = "html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
+		vim.g.closetag_xhtml_filetypes =
+			"html,xhtml,razor,typescriptreact,javascriptreact,typescript.tsx,javascript.tsx"
+		vim.g.closetag_regions = {
+			["typescript.tsx"] = "jsxRegion,tsxRegion",
+			["javascript.jsx"] = "jsxRegion",
+			["typescriptreact"] = "jsxRegion,tsxRegion",
+			["javascriptreact"] = "jsxRegion",
+		}
+		vim.g.closetag_shortcut = ">"
+	end,
+}
